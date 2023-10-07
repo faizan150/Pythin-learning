fd0ac73a2bc1d00f8f019e590d7f90b14f12eb61
Muhammad Faizan <faizi12570@gmail.com>
2023-10-07 12:23:31 +0500

    07*10*2023 Python Learning

---
+++ main.py
@@ -0,0 +1,15 @@
+name : str = 'Muhammad Faizan'
+fatherName : str = 'Abdul Rasheed'
+age : int = 22
+education : str = 'BSSE'
+
+card : str = f"""
+    PIAIC Student Card
+    Student name:\t{name}
+    FatherName:\t{fatherName}
+    Age:\t{age}
+    Education:\t{education}
+    Total:\t{120 + 130}
+"""
+
+print(card)
